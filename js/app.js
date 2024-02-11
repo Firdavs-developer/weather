6d289a5b8e4b6b5b35359c42cdf740ab37ffe39b
Firdavs-developer <firdavsdosiyorov3@gmail.com>
2024-02-11 13:59:04 +0500

    Finished

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let elTemp = document.querySelector(".temp");
 let elForm = document.querySelector(".form");
 let elFormInput = document.querySelector(".form-input") ;
 let elBtnForm = document.querySelector(".btn-form");
+let elWeatherImg = document.querySelector(".weather-img");
 let API_KEY = "3babf5305e2fdf755d46d99de1f887f7"
 
 
@@ -12,10 +13,16 @@ async function  checkWeather(city){
     let data = await response.json();
     console.log(data);
     elCountry.textContent = data.name;
-    elTemp.textContent = `${Math.round(data.main.temp)}^C`
+    if(data.weather[0].description == "clear sky"){
+        elWeatherImg.src = "../images/sunny.jpg";
+    }else if(data.weather[0].description == "broken clouds"){
+        elWeatherImg.src = "../images/cloudly.jpg";
+    }
+    elTemp.textContent = `${Math.round(data.main.temp)}^C`;
 } 
 
 elForm.addEventListener("submit", (evt) => {
     evt.preventDefault();
+
     checkWeather(elFormInput.value.trim());
 })
